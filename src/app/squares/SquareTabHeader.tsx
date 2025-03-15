fca0bf145259049451c61b797326264e1b4f9822
michael <michaelslack42@gmail.com>
2025-03-15 21:43:09 +0000

    Context menu demo

--- src/app/squares/SquareTabHeader.tsx
+++ src/app/squares/SquareTabHeader.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "@/store/hooks";
 import { selectHasPluralPanels } from "@/store/slices/squaresSlice";
 import { IDockviewPanelHeaderProps } from "dockview";
 import { X } from "lucide-react";
-import { ConfigurableContext } from "../configurableContextMenu/ConfigurableContext";
+import { ConfigurableContext, ContextMenuConfiguration } from "../configurableContextMenu/ConfigurableContext";
 
 export function SquareTabHeader(props: IDockviewPanelHeaderProps) {
   const onCloseClick = () => {
@@ -11,8 +11,58 @@ export function SquareTabHeader(props: IDockviewPanelHeaderProps) {
 
   const canClose = useAppSelector(selectHasPluralPanels);
 
+  const contextConfiguration:  ContextMenuConfiguration = {
+    items: [
+        {
+            title: "Rename tab",
+            actionId: "notimplemented",
+            itemType: "action"
+        },
+        {
+            title: "Duplicate tab",
+            actionId: "notimplemented",
+            itemType: "action"
+        },
+        {
+            title: "Transform tab",
+            actionId: "notimplemented",
+            itemType: "action"
+        },
+        {
+            itemType: "separator"
+        },
+        {
+            title: "Test Submenu",
+            itemType: "submenu",
+            submenu: [
+                {
+                    title: "Test action",
+                    actionId: "notimplemented",
+                    itemType: "action"
+                },
+                {
+                    title: "Test subsubmenu",
+                    itemType: "submenu",
+                    submenu: [
+                        {
+                            title: "Test action",
+                            actionId: "notimplemented",
+                            itemType: "action"
+                        },
+                        {
+                            title: "Test action",
+                            actionId: "notimplemented",
+                            itemType: "action"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+  }
+
   return (
-    <ConfigurableContext configuration={{}}>
+    <ConfigurableContext configuration={contextConfiguration}>
       <div className="bg-amber-400 h-full w-full px-1 flex flex-row">
         <div>{props.api.title}</div>
         {canClose ? <X onClick={onCloseClick} /> : <></>}
