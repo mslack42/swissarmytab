6c9e68f3c6e1791f7f2c37ccd78fac1b1bbe8223
michael <michaelslack42@gmail.com>
2025-03-23 22:22:24 +0000

    add router for local dev

---
+++ src/app/dev/TestBodyEditor.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router"
+import { SquareBodyTriageComponent } from "../userInterface/squares/SquareBodyTriageComponent"
+
+export function TestBodyEditor()
+{
+    const params = useParams()
+    return <SquareBodyTriageComponent id={params.panelId!}/>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import store from './store/redux/store.ts'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import store from "./store/redux/store.ts";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { TestBodyEditor } from "./app/dev/TestBodyEditor.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* Turn this route off in production */}
+          <Route path="/develop/editor/:panelId" element={<TestBodyEditor/>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
