d51615c796efec32813aff2352102d1848cee078
michael <michaelslack42@gmail.com>
2025-04-02 22:38:30 +0100

    remove WIP now that version is available

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -16,7 +16,6 @@ export function TopBar() {
         <div className="w-full text-white p-1 font-bold flex">
           <div className="text-left flex-1 overflow-x-ellipsis text-ellipsis max-h-14 text-nowrap">
             <h1 className="text-3xl">SWISS ARMY TAB</h1>
-            <p className="text-sm">(a work in progress utility site)</p>
           </div>
           <div className="text-right flex-none p-2 text-lg">
             <Settings onClick={openSettings} />
