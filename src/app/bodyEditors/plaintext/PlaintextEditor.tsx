9965b3d44a4301f60dc8cb530c7c038f9657690f
michael <michaelslack42@gmail.com>
2026-08-19 19:45:34 +0100

    Plain text data

--- src/app/bodyEditors/plaintext/PlaintextEditor.tsx
+++ src/app/bodyEditors/plaintext/PlaintextEditor.tsx
@@ -1,12 +1,23 @@
 import { useAppSelector } from "@/store/redux/hooks";
 import { selectPanelData } from "@/store/redux/slices/squaresSlice";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { dataService } from "@/store/services/dataService";
 import { Textarea } from "@/components/ui/textarea";
 import { BodyEditorProps } from "../../userInterface/squares/SquareBodyTriageComponent";
 
 export function PlaintextEditor(props: BodyEditorProps) {
-  const data = useAppSelector(selectPanelData(props.id));
-  const [bodyText, setBodyText] = useState("dataId: " + data?.dataId);
+  const panelData = useAppSelector(selectPanelData(props.id));
+  const data = dataService.getData(panelData!.dataId);
+  const [bodyText, setBodyText] = useState(
+    data?.dataType == "text" ? data.content : "",
+  );
+
+  useEffect(() => {
+    dataService.updateData(panelData!.dataId!, {
+      dataType: "text",
+      content: bodyText,
+    });
+  }, [bodyText, panelData]);
 
   return (
     <>
