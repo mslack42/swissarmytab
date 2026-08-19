1a713b1ba19b77616646fefb2b929ab84118a865
michael <michaelslack42@gmail.com>
2026-08-19 22:00:15 +0100

    KeepAwake copy button

--- src/app/bodyEditors/keepAwakeEditor/KeepAwakeEditor.tsx
+++ src/app/bodyEditors/keepAwakeEditor/KeepAwakeEditor.tsx
@@ -10,7 +10,7 @@ export function KeepAwakeEditor() {
 
   const toggleSleep = (newSleep: boolean) => {
     setSleep(newSleep);
-    if (newSleep) {
+    if (!newSleep) {
       noSleepHandler.enable();
     } else {
       noSleepHandler.disable();
@@ -25,27 +25,53 @@ while (1) {\n\
 
   return (
     <div>
-      <button onClick={() => toggleSleep(!sleep)}>
-        <div
-          className={
-            "p-1 px-2 rounded-lg" + (sleep ? " bg-green-500 " : " bg-red-500")
-          }
-        >
-          {sleep ? (
-            <p>Press the button to release the wake lock</p>
-          ) : (
-            <p>Press the button prevent your computer from going to sleep</p>
-          )}
-        </div>
-      </button>
-      <h2 className="italic text-sm">Not working for you?</h2>
-      <p className="italic text-sm">
-        If Powershell is an option, you could try the following script, which
-        simulates F15 button presses at regular intervals
+      <h2 className="text-lg">Powershell</h2>
+      <p className="text-sm">
+        The following script simulates F15 button presses at regular intervals
       </p>
       <SyntaxHighlighter language="powershell" style={a11yDark} showLineNumbers>
         {powershellScript}
       </SyntaxHighlighter>
+      <div className="p-3 w-full flex flex-row justify-center">
+        <CopyButton text={powershellScript} />
+      </div>
+      <br />
+      <h2 className="text-lg">Browser (WIP)</h2>
+      <p className="text-sm">
+        Toggling this button runs some code in your browser in an attempt to
+        keep the window active.
+      </p>
+      <div className="p-3 w-full flex flex-row justify-center">
+        <button onClick={() => toggleSleep(!sleep)}>
+          <div
+            className={
+              "p-1 px-2 rounded-lg" +
+              (!sleep ? " bg-green-500 " : " bg-red-500")
+            }
+          >
+            {sleep ? <p>Sleeping...</p> : <p>AWAKE</p>}
+          </div>
+        </button>
+      </div>
     </div>
+  );
+}
+
+function CopyButton({ text }: { text: string }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
+  return (
+    <button
+      onClick={handleCopy}
+      className="w-24 bg-slate-500 hover:bg-slate-300 rounded-sm p-1"
+    >
+      {copied ? "Copied!" : "Copy"}
+    </button>
   );
 }
